4c02c7244943deeca9dfa79546dc3ff6b6c0d01f
Anastasia Gorshkova <anastasia.gorshkova@simbirsoft.com>
2018-12-01 02:03:17 +0400

    actions for genie

--- frontend/src/Agent/index.js
+++ frontend/src/Agent/index.js
@@ -1,11 +1,76 @@
-export default class Agent {
-  constructor(agent) {
-    this.agent = agent;
-    console.log(this.agent)
-    document.addEventListener('click', this.Move.bind(this), false);
+import {getOffset, limit} from '@/utils'
+
+const AGENT_WIDTH = 128
+const AGENT_HEIGHT = 128
+
+class Agent {
+  constructor() {
+    clippy.load('Genie', (agent) => {
+      this.agent = agent;
+      agent.show();
+    });
   }
-  
-  Move() {
-    this.agent.moveTo(0,0);
+
+  moveToNavbar() {
+    this.moveTo('navbar')
+  }
+
+  moveToHeader() {
+    this.moveTo('header')
+  }
+
+  moveToFooter() {
+    this.moveTo('footer')
+  }
+
+  moveToContent() {
+    this.moveTo('content')
+  }
+
+  moveToContent() {
+    this.moveTo('content')
+  }
+
+  getCoordinates(element) {
+    const {top, left, bottom, right, centerX, centerY} = getOffset(element)
+    const app = document.getElementById('app')
+    const {width: appWidth, height: appHeight} = app.getBoundingClientRect()
+    const centerYCoordinates = centerY - AGENT_HEIGHT/2
+    const centerXCoordinates = centerX - AGENT_WIDTH/2
+    if (appWidth - right >= AGENT_WIDTH) {
+      return {x: right, y: centerYCoordinates}
+    }
+    if (appHeight - bottom >= AGENT_WIDTH) {
+      return {x: centerXCoordinates, y: bottom}
+    }
+    if (left >= AGENT_WIDTH) {
+      return {x: left - AGENT_WIDTH, y: centerYCoordinates}
+    }
+    if (top >= AGENT_WIDTH) {
+      return {x: centerXCoordinates, y: top - AGENT_HEIGHT}
+    }
+    return {x: centerXCoordinates, y: centerYCoordinates}
+  }
+
+  moveTo(id) {
+    if (!this.agent) {
+      return
+    }
+    const element = document.getElementById(id);
+    if (element) {
+      const {x, y} = this.getCoordinates(element)
+      this.agent.moveTo(x, y);
+    } else {
+      this.agent.play('Confused')
+    }
+  }
+
+  speak(phrase) {
+    if (!this.agent) {
+      return
+    }
+    this.agent.speak(phrase);
   }
 }
+
+export default new Agent()
--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,23 @@
 <template>
     <div id="app">
-        <img src="./assets/logo.png">
-        <router-view/>
+        <!-- <img src="./assets/logo.png">
+        <router-view/> -->
+        <div class="header"><button id="header" @click="moveToHeader">Header</button></div>
+        <div class="container">
+            <div class="navbar" ><button id="navbar" @click="moveToNavbar">Navbar</button></div>
+            <div class="content">
+                <button id="content" @click="moveToContent">Content</button>
+                <button id="content" @click="moveToUnknown">Unknown</button>
+                <button id="content" @click="speak">Speak</button>
+            </div>
+        </div>
+        <div class="footer"><button id="footer" @click="moveToFooter">Footer</button></div>
     </div>
 </template>
 
 <script>
     import io from 'socket.io-client';
+    import Agent from '@/Agent'
 
     const socket = io('http://b8473681.ngrok.io/');
 
@@ -18,20 +29,56 @@
     });
     socket.on('disconnect', () => {});
     export default {
-        name: 'App'
+        name: 'App',
+        methods: {
+            moveToNavbar: () => Agent.moveToNavbar(),
+            moveToContent: () => Agent.moveToContent(),
+            moveToFooter: () => Agent.moveToFooter(),
+            moveToHeader: () => Agent.moveToHeader(),
+            moveToUnknown: () => Agent.moveTo('unknown'),
+            speak: () => Agent.speak('Hey! Wana make a cool web page? Then yoy came to the right place!'),
+            // unk: () => Agent.moveToHeader(),
+        }
     }
 </script>
 
 <style>
+html, body {
+    height: 100%;
+    margin: 0;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+  min-height: 100%;
 }
-.clippy {
+.header {
+    border: 1px solid black;
+    min-height: 100px;
+}
+.footer {
+    border: 1px solid black;
+    min-height: 100px;
+}
+.container {
+  display: flex;
+  flex-direction: row;
+  flex-grow: 1;
+}
+.navbar {
+    border: 1px solid black;
+    min-width: 100px;
+}
+.content {
+    border: 1px solid black;
+    flex-grow: 1;
+}
+/* .clippy, .clippy-balloon {
   position: absolute
-}
+} */
 </style>
