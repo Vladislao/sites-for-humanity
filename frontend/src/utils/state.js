dbfea11a6d09cd74ae830d6027431c5237feb9af
vladislao <netcemetery@yandex.ru>
2018-12-01 02:53:04 +0400

    add default state for header

--- frontend/src/utils/state.js
+++ frontend/src/utils/state.js
@@ -1,9 +1,30 @@
 export const dispatch = (state, actions) => {
   console.log(actions);
   console.log(state);
+
   return { ...state };
 };
 
 export default {
-  some: "default state"
+  header: {
+    display: false,
+    nav: {
+      display: false,
+      brand: {
+        display: false,
+        text: "",
+        imgSrc:
+          "https://upload.wikimedia.org/wikipedia/commons/5/53/Vue.js_Logo.svg"
+      },
+      nav: {
+        display: false,
+        items: [
+          {
+            text: "Home",
+            url: "/"
+          }
+        ]
+      }
+    }
+  }
 };
